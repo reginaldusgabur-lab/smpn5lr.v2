9fca5ed8b348a4530508b760a53de42199f7aadd
reginaldusgabur <reginaldusgabur@gmail.com>
2026-06-26 15:09:59 +0000

    fix: eliminate pwa white flash on startup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const viewport: Viewport = {
   viewportFit: 'cover',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#FFFFFF' },
-    { media: '(prefers-color-scheme: dark)', color: '#000000' }
+    { media: '(prefers-color-scheme: dark)', color: '#10172A' }
   ],
 }
 
