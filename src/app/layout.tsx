9520a37daec3a9f56e3bc81337fc896ca3ccf603
Reginaldus Gabur <reginaldusgabur@gmail.com>
2026-06-27 18:47:31 +0000

    pada perangkat android versi 8.1 tidak bisa buka aplikasi PWA kalau di b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/logo-3d-v2.png" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased bg-background text-foreground" suppressHydrationWarning>
         <ThemeProvider
